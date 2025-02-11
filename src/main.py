26d5928f5b958d1377e38e77b8ef512d851722e2
Bianca <99222703+BrytanniADJ@users.noreply.github.com>
2025-02-11 16:52:29 -0300

    Update 11-02-25/1

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from sqlite3 import Cursor
 import mysql.connector
 from mysql.connector import Error
 from PyQt6 import uic, QtWidgets, QtCore, QtGui
-from PyQt6.QtWidgets import (
+from PyQt6.QtWidgets import(
     QFileDialog, QApplication, QWidget, 
     QPushButton, QTableWidget, 
     QErrorMessage, QTimeEdit, QMainWindow
@@ -20,21 +20,28 @@ from gif_loader import add_gif_to_label
 
 app = QtWidgets.QApplication([])
 
+def new_note():
+    post.show()
+
 def main_close():
     main.close()
     note.show()
 
 def inicio():
-    main.show() # Pausar por 50 segundos antes de mudar de janela
-    QTimer.singleShot(50000, main_close)
+    main.show() # Pausar por 20 segundos antes de mudar de janela
+    QTimer.singleShot(5000, main_close)
 
 # .ui
 main = uic.loadUi('.ui/main.ui')
 note = uic.loadUi('.ui/note.ui')
+post = uic.loadUi('.ui/post.ui')
 
 # .gif_loader
 label = main.findChild(QtWidgets.QLabel, "Qmove")
 add_gif_to_label(label, "./assets/img/icons8-spinner.gif")
+
+# botoes
+add_note = note.add_note.clicked.connect(new_note)
 
 inicio()
 app.exec()
